e3c519a8fa274dbc75fc298e41d2d14716907f0a
)- <a-@-s-MacBook-Pro.local>
2022-06-19 19:49:05 -0400

    wip index and show route added to main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,25 @@
+//component libraries
+import{Route} from 'react-router-dom';
+
+//page components
+import Index from '../pages/Index';
+import Show from '../pages/Show'
+
 const Main = (props) => {
-    return <h1>Main Component</h1>
+    return (
+        <main>
+           
+           <Route exact path="/">
+            <Index />
+           </Route>
+
+           {/* (rp)render props passes down 3 properties, history, match, location */}
+           {/* ...rp is props spreding */}
+           <Route path="/people/:id" render={(rp) => <Show {...rp} />} />
+            
+
+        </main>
+    )
 }
 
 export default Main;
